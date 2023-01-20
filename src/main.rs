5d277734121542f40576ad1d521f11ae26876205
NicolasHov <hovart.nicolas@gmail.com>
2023-01-20 17:57:44 +0100

    Add non-persistent CRUD

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,112 @@
-use rocket::request::FlashMessage;
-use rocket::response::{Flash, Redirect};
-use rocket::form::FromForm;
 use rocket::form::Form;
+use rocket::form::FromForm;
+use rocket::http::Status;
+use rocket::response::content::RawHtml;
+use rocket::response::{Flash, Redirect};
+use rocket::Request;
+use rocket::*;
 
-#[macro_use]
+// #[macro_use]
 extern crate rocket;
 
-// ---------------------GET with params
+// --------------------- GET Index
+
+#[get("/")]
+fn index() -> RawHtml<&'static str> {
+    RawHtml(include_str!("templates/index.html"))
+}
+
+// --------------------- GET : Account
+
+#[get("/show_form")]
+fn show_form() -> RawHtml<&'static str> {
+    return RawHtml(include_str!("templates/account.html"));
+}
+
+//  --------------------POST : create account
+
+#[derive(FromForm)]
+struct CreateAccount<'r> {
+    name: &'r str,
+}
+
+#[post("/create_account", data = "<account>")]
+fn create_account(account: Form<CreateAccount<'_>>) -> Flash<Redirect> {
+    println!("{:#?}", account.name);
+    // let mut name = account.name;
+    Flash::success(Redirect::to(uri!("/")), account.name)
+    // return RawHtml(include_str!("templates/account.html"));
+}
+
+// --------------------- DELETE : Account (TO TEST)
+
+#[delete("/delete_account/<name>")]
+pub fn delete_account(name: &str) -> String {
+    format!("{} deleted", name)
+    // Flash::success(Redirect::to(uri!("/")), format!("{} deleted", name))
+}
+// ---------------------GET with route params
 
 #[get("/hello/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
-// --------------------- POST with params
+//TOFIX: --------------------- GET with query params
 
-#[get("/")]
-fn index(flash: Option<FlashMessage<'_>>) -> String {
-    flash.map(|flash| format!("{}: {}", flash.kind(), flash.message()))
-         .unwrap_or_else(|| "Welcome!".to_string())
+#[get("/math?<first>&<second>")]
+fn multiply(first: i32, second: i32) -> String {
+    //   String::from(
+    format!("{}", first * second)
+    // );
 }
 
-#[post("/login/<name>")]
-fn login(name: &str) -> Result<&'static str, Flash<Redirect>> {
-    if name == "special_user" {
-        Ok("Hello, special user!")
-    } else {
-        Err(Flash::error(Redirect::to(uri!(index)), "Invalid username."))
-    }
+// -------------------- (POST with params)
+
+// #[post("/login/<name>")]
+// fn login(name: &str) -> Result<&'static str, Flash<Redirect>> {
+//     if name == "special_user" {
+//         Ok("Hello, special user!")
+//     } else {
+//         Err(Flash::error(Redirect::to(uri!(index)), "Invalid username."))
+//     }
+// }
+
+//  ----------------------- Catchers
+
+#[catch(500)]
+fn internal_error() -> &'static str {
+    "Whoops! Looks like we messed up."
 }
 
-
-//  --------------------POST with data
-
-#[derive(FromForm)]
-struct Task<'r> {
-   #[field(validate = len(1..))]
-   description: &'r str,
-   completed: bool
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("I couldn't find '{}'. Try something else?", req.uri())
 }
 
-#[post("/", data = "<task>")]
-fn new(task: Form<Task<'_>>) -> Flash<Redirect> {
-    println!("{:#?}",task.description);
-    Flash::success(Redirect::to(uri!(index)), "Task added.")
+#[catch(default)]
+fn default(status: Status, req: &Request) -> String {
+    format!("{} ({})", status, req.uri())
 }
 
-//  -----------------------
+// ------------------------
 
 #[launch]
 fn rocket() -> _ {
+    // let not_found_catcher = Catcher::new(404, handle_404);
+    // let internal_server_error_catcher = Catcher::new(500, handle_500);
     rocket::build()
-        .mount("/", routes![index, hello, login, new])
-        .mount("/hi", routes![index])
+        .mount(
+            "/",
+            routes![
+                index,
+                hello,
+                multiply, // login,
+                show_form,
+                create_account,
+                delete_account
+            ],
+        )
+        // .mount("/account", routes![show_form, create_account])
+        .register("/", catchers![internal_error, not_found, default])
 }
